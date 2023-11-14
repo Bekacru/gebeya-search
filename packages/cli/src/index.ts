8f5b2e03ede2d4f02b7c3956e4e1eb2fd5263df2
bekacru <bekacru@gmail.com>
2023-11-14 10:16:26 +0300

    feat: schema

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,16 +1,13 @@
 import { Command } from "commander";
 
 import { Telegram } from "./telegram";
-import { logger } from "./utils/logger";
 
-async function main() {
-  const program = new Command()
-    .name("gebeyasearch setup cli")
-    .description("Bootstrap the project");
-  program.addCommand(Telegram);
-  program.parse();
+function main() {
+    const program = new Command()
+        .name("gebeyasearch setup cli")
+        .description("Bootstrap the project");
+    program.addCommand(Telegram);
+    program.parse();
 }
 
-main().catch((e) => {
-  logger.error(e);
-});
+main();
--- packages/cli/src/telegram.ts
+++ packages/cli/src/telegram.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Command } from "commander";
 import prompts from "prompts";
 import { TelegramClient } from "telegram";
@@ -7,65 +8,69 @@ import { z } from "zod";
 import { logger } from "./utils/logger";
 
 const schema = z.object({
-  forceSms: z.boolean().optional(),
+    forceSms: z.boolean().optional(),
 });
 
 export const Telegram = new Command()
-  .name("telegram")
-  .description("This wil setup telegram session for you")
-  .option("--force-sms", "Force SMS for verification code")
-  .action(async (opts) => {
-    const data = schema.parse(opts);
-    const apiId = process.env.TELEGRAM_API_ID;
-    const apiHash = process.env.TELEGRAM_API_HASH;
-    if (!apiId || !apiHash) {
-      logger.error("Please add API Id and API Hash on the root .env file.");
-      process.exit(1);
-    }
-    const client = new TelegramClient(
-      new StringSession(""),
-      parseInt(apiId),
-      apiHash,
-      { connectionRetries: 5 },
-    );
-    const options = await prompts({
-      type: "text",
-      name: "phoneNumber",
-      message: "Please input your phone number: ",
+    .name("telegram")
+    .description("This wil setup telegram session for you")
+    .option("--force-sms", "Force SMS for verification code")
+    .action(async (opts) => {
+        const data = schema.parse(opts);
+        const apiId = process.env.TELEGRAM_API_ID;
+        const apiHash = process.env.TELEGRAM_API_HASH;
+        if (!apiId || !apiHash) {
+            logger.error(
+                "Please add API Id and API Hash on the root .env file.",
+            );
+            process.exit(1);
+        }
+        const client = new TelegramClient(
+            new StringSession(""),
+            parseInt(apiId),
+            apiHash,
+            { connectionRetries: 5 },
+        );
+        // await prompts({
+        //     type: "text",
+        //     name: "phoneNumber",
+        //     message: "Please input your phone number: ",
+        // });
+        await client.start({
+            phoneNumber: async () =>
+                (
+                    await prompts({
+                        type: "text",
+                        name: "phoneNumber",
+                        message: "Please Input Your Phone Number: ",
+                    })
+                ).phoneNumber,
+            password: async () =>
+                (
+                    await prompts({
+                        type: "text",
+                        name: "password",
+                        message: "Please Input Your Password: ",
+                    })
+                ).password,
+            phoneCode: async (isCodeViaApp) =>
+                (
+                    await prompts({
+                        type: "text",
+                        name: "code",
+                        message: "Please Input the code: ",
+                        hint: isCodeViaApp
+                            ? "Verification code has been sent to your telegram app"
+                            : "Verification code has been sent to your phone",
+                    })
+                ).code,
+            onError: (err) => logger.error(err),
+            forceSMS: data.forceSms,
+        });
+        logger.success("Done!");
+        logger.info(
+            `Save the string below in the root .env file\n\nTELEGRAM_SESSION=${
+                client.session.save() as unknown as string
+            }`,
+        );
     });
-    await client.start({
-      phoneNumber: async () =>
-        (
-          await prompts({
-            type: "text",
-            name: "phoneNumber",
-            message: "Please Input Your Phone Number: ",
-          })
-        ).phoneNumber,
-      password: async () =>
-        (
-          await prompts({
-            type: "text",
-            name: "password",
-            message: "Please Input Your Password: ",
-          })
-        ).password,
-      phoneCode: async (isCodeViaApp) =>
-        (
-          await prompts({
-            type: "text",
-            name: "code",
-            message: "Please Input the code: ",
-            hint: isCodeViaApp
-              ? "Verification code has been sent to your telegram app"
-              : "Verification code has been sent to your phone",
-          })
-        ).code,
-      onError: (err) => logger.error(err),
-      forceSMS: data.forceSms,
-    });
-    logger.success("Done!");
-    logger.info(
-      `Save the string below in the root .env file\n\nTELEGRAM_SESSION=${client.session.save()}`,
-    );
-  });
